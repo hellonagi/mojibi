ede9da98458112d0406e0dcaa97adb5c60985a5e
n2 <38994662+n2dev@users.noreply.github.com>
2023-03-28 19:10:09 +0900

    Fix minor things

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom'
-import { render, screen, fireEvent, within, waitFor } from '@testing-library/react'
+import { render, screen, fireEvent, within } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import App from './App'
 
