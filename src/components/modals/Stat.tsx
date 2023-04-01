cc1a28d67280ff0a614097cad3a521411a20a0e5
n2 <nagisa.n102@gmail.com>
2023-04-01 22:37:42 +0900

    Change modal to Dialog

--- src/components/modals/Stat.tsx
+++ src/components/modals/Stat.tsx
@@ -1,26 +1,16 @@
 import { useContext } from 'react'
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
-import Modal from '@mui/material/Modal'
+import Dialog from '@mui/material/Dialog'
+import DialogContent from '@mui/material/DialogContent'
 import Typography from '@mui/material/Typography'
 import Divider from '@mui/material/Divider'
 import ShareOutlinedIcon from '@mui/icons-material/ShareOutlined'
+import IconButton from '@mui/material/IconButton'
+import CloseIcon from '@mui/icons-material/Close'
 
 import { mojibiState, mojibiStats, HeaderContext } from '../../App'
 import StatBarChart from '../charts/StatBarChart'
-
-const style = {
-	position: 'absolute',
-	top: '50%',
-	left: '50%',
-	transform: 'translate(-50%, -50%)',
-	width: 400,
-	backgroundColor: 'background.paper',
-	borderRadius: 2,
-	border: '1px solid #222',
-	boxShadow: 24,
-	p: 4,
-}
 
 interface StatContext {
 	openStat: boolean
@@ -60,13 +50,33 @@ const Stat = () => {
 	}
 
 	return (
-		<Modal
+		<Dialog
 			open={openStat}
 			onClose={handleClose}
-			aria-labelledby='parent-modal-title'
-			aria-describedby='parent-modal-description'
+			PaperProps={{
+				style: { borderRadius: 12 },
+			}}
 		>
-			<Box sx={{ ...style, width: 400 }}>
+			<IconButton
+				aria-label='close'
+				onClick={handleClose}
+				sx={{
+					position: 'absolute',
+					right: 8,
+					top: 8,
+					color: (theme) => theme.palette.grey[500],
+				}}
+			>
+				<CloseIcon />
+			</IconButton>
+			<DialogContent
+				sx={{
+					width: 400,
+					boxShadow: 24,
+					backgroundColor: 'background.paper',
+					p: 4,
+				}}
+			>
 				<Typography my={1} variant='h2' component='h2'>
 					本日の結果
 				</Typography>
@@ -101,8 +111,8 @@ const Stat = () => {
 						シェア
 					</Button>
 				</Box>
-			</Box>
-		</Modal>
+			</DialogContent>
+		</Dialog>
 	)
 }
 
