3dd40f51b195921f09584a648491ae1113cca269
n2 <nagisa.n102@gmail.com>
2023-04-03 15:45:37 +0900

    Change the algorithm for generating bingo characters

--- src/utils/getBingoChars.ts
+++ src/utils/getBingoChars.ts
@@ -1,39 +1,47 @@
 /**
  * Bingo Characters
- * It excludes ぢ、を, ゎ because there's few words that contain them
+ * The characters ぢ, を, and ゎ are excluded because there's few words that contain them
  */
 export const hiraganaNormal =
 	'あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわん'
-export const hiraganaSpecial = 'ぁぃぅぇぉがぎぐげござじずぜぞだづでどっばびぶべぼぱぴぷぺぽゃゅょー'
+export const hiraganaSpecial =
+	'ぁぃぅぇぉがぎぐげござじずぜぞだづでどっばびぶべぼぱぴぷぺぽゃゅょー'
 
 /**
  * Generates an array of random hiragana characters without duplicates (length = 25).
  * The ratio of normal characters to special ones depends on the difficulty
  * (Normal: 20:5 Hard: 18:7).
  *
- * @param seed - The seed value comes from today's date
- * @param difficulty - 'NORMAL' | 'HARD'
- * @returns An array of 25 string elements
+ * @param {number} seed - The seed value comes from today's date
+ * @param {string} difficulty - 'NORMAL' | 'HARD'
+ * @returns {string[]} - An array of random hiragana characters
  * @example
  * ```
- * // ['す', 'い', 'り', 'ど', 'う',
- * //  'よ', 'あ', 'う', 'づ', 'と',
- * //  'た', 'ぽ', 'お', 'け', 'ぼ',
- * //  'ろ', 'や', 'ぬ', 'し', 'の',
- * //  'ゃ', 'ふ', 'れ', 'そ', 'ほ',]
- * console.log(getBingoChars())
+ * // ['へ', 'づ', 'あ', 'ら', 'も',
+ * //  'お', 'ふ', 'ぜ', 'ぬ', 'ん',
+ * //  'む', 'ろ', 'く', 'げ', 'つ',
+ * //  'ざ', 'さ', 'よ', 'る', 'が',
+ * //  'え', 'や', 'め', 'た', 'き']
+ * console.log(getBingoChars(20230401))
  * ```
  */
 export const getBingoChars = (seed: number, difficulty = 'NORMAL'): string[] => {
-	const _pseudoRandom = () => {
-		const x = Math.sin(seed) * 1337
-		return x - Math.floor(x)
+	function _lcg(seed: number) {
+		const a = 1664525
+		const c = 1013904223
+		const m = Math.pow(2, 32)
+		let state = seed
+
+		return function random() {
+			state = (a * state + c) % m
+			return state / m
+		}
 	}
 
-	const _shuffle = (array: string[]) => {
+	function _seededShuffle(array: string[], seed: number) {
+		const prng = _lcg(seed)
 		for (let i = array.length - 1; i > 0; i--) {
-			const j = Math.floor(_pseudoRandom() * (i + 1))
-
+			const j = Math.floor(prng() * (i + 1))
 			;[array[i], array[j]] = [array[j], array[i]]
 		}
 		return array
@@ -44,9 +52,12 @@ export const getBingoChars = (seed: number, difficulty = 'NORMAL'): string[] => 
 		ratio = [18, 7]
 	}
 
-	const bingoNormalChars = _shuffle(hiraganaNormal.split('')).slice(0, ratio[0])
-	const bingoSpecialChars = _shuffle(hiraganaSpecial.split('')).slice(0, ratio[1])
-	const bingoChars = _shuffle(bingoNormalChars.concat(bingoSpecialChars))
+	const bingoNormalChars = _seededShuffle(hiraganaNormal.split(''), seed).slice(0, ratio[0])
+	const bingoSpecialChars = _seededShuffle(hiraganaSpecial.split(''), seed + 100000).slice(
+		0,
+		ratio[1]
+	)
+	const bingoChars = _seededShuffle(bingoNormalChars.concat(bingoSpecialChars), seed + 200000)
 
 	return bingoChars
 }
